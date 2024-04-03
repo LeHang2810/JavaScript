64fe881d0538ad80aac9b480b3ec8c8a540071a7
Le Hang <tranthilehang3012v@gmail.com>
2024-04-03 22:40:02 +0700

    +update_tuan3

--- Bai_tap_tuan3/SNT.js
+++ Bai_tap_tuan3/SNT.js
@@ -1,15 +1,36 @@
-var x = 3;
-var count = 0;
+// var x = 3;
+// var count = 0;
 
-for(var i = 0; i < x - 2; i++) {
-    if(x % i == 0) {
-        console.log("Uoc so khac: " ,i);
-        count++;
-        break;
-    }
+// for(var i = 0; i < x - 2; i++) {
+//     if(x % i == 0) {
+//         console.log("Uoc so khac: " ,i);
+//         count++;
+//         break;
+//     }
+// }
+// if(count == 0) {
+//     console.log("Day la so nguyen to")
+// }else {
+//     console.log("Day khong phai la so nguyen to")
+// }
+
+//
+function check(n){
+    if( n<=1){
+        return false;
 }
-if(count == 0) {
-    console.log("Day la so nguyen to")
-}else {
-    console.log("Day khong phai la so nguyen to")
+for (let i =2; i*i<=n; i++){
+    if( n % i === 0){
+        return false;
+    }
+    return true;
+
+}
+
+let x = 3;
+if(check(x)){
+    console.log(x + 'la so nguyn to');
+} else{
+    console.log( x + 'khong phai la so nguyen to')
+}
 }
